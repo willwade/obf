ef090fe9a4036e3a1fc71a2cdc0bbe2af6a381db
will wade <willwade@gmail.com>
2023-09-26 00:40:53 +0100

    Update grid3.rb
    NB: Wont work

--- lib/obf/grid3.rb
+++ lib/obf/grid3.rb
@@ -7,51 +7,67 @@ require 'fileutils'
 
 module OBF
   class Grid3
-    # Class variables and methods
-
     # Convert from Grid 3 gridset to OBF's internal format
     def self.to_external(gridset_path)
-      # Read and unzip the Grid 3 gridset file
-      # Extract the XML files (settings.xml, styles.xml, and individual grid.xml files)
-      # Initialize an empty hash to store the parsed data
-      parsed_data = {}
+      # Initialize a hash to store the parsed data
+      parsed_data = {
+        settings: nil,
+        styles: nil,
+        grids: {}
+      }
 
-      # Read settings.xml and parse it
-      parsed_data[:settings] = parse_grid3_settings('path/to/settings.xml')
+      # Unzip the Grid3 gridset into a temporary folder
+      unzip_gridset(gridset_path, '/tmp/grid3_temp')
 
-      # Read styles.xml and parse it
-      parsed_data[:styles] = parse_grid3_styles('path/to/styles.xml')
+      # Parse settings and styles
+      parsed_data[:settings] = parse_grid3_settings('/tmp/grid3_temp/path/to/settings.xml')
+      parsed_data[:styles] = parse_grid3_styles('/tmp/grid3_temp/path/to/styles.xml')
 
-      # Loop through each grid folder and read grid.xml, then parse it
-      parsed_data[:grids] = {}
-      Dir.foreach('path/to/Grids') do |folder_name|
+      # Loop through each grid folder and read grid.xml
+      Dir.foreach('/tmp/grid3_temp/path/to/Grids') do |folder_name|
         next if ['.', '..'].include? folder_name
-        grid_path = "path/to/Grids/#{folder_name}/grid.xml"
+        grid_path = "/tmp/grid3_temp/path/to/Grids/#{folder_name}/grid.xml"
         parsed_data[:grids][folder_name] = Nokogiri::XML(File.read(grid_path))
       end
 
       # Convert to OBF's internal format using map_grid3_to_obf
       obf_internal_format = map_grid3_to_obf(parsed_data[:settings], parsed_data[:styles], parsed_data[:grids])
 
-      # Return the OBF internal format
-      return obf_internal_format
+      # Cleanup temporary folder
+      FileUtils.rm_rf('/tmp/grid3_temp')
+
+      # Return the internal content
+      obf_internal_format
+    end
+
+    # Helper function to unzip Grid3 gridset
+    def self.unzip_gridset(zip_path, extract_to)
+      Zip::File.open(zip_path) do |zip_file|
+        zip_file.each do |f|
+          f_path = File.join(extract_to, f.name)
+          FileUtils.mkdir_p(File.dirname(f_path))
+          f.extract(f_path)
+        end
+      end
     end
 
     # Helper functions for XML parsing, style mapping, etc.
-    def self.parse_grid3_settings(settings_xml)
-      # Parse settings from Grid 3's settings.xml
+    def self.parse_grid3_settings(settings_path)
+      # Parse settings from Grid 3's settings.xml using Nokogiri
+      settings_xml = Nokogiri::XML(File.read(settings_path))
       # Convert to a hash and return
     end
 
-    def self.parse_grid3_styles(styles_xml)
-      # Parse styles from Grid 3's styles.xml
+    def self.parse_grid3_styles(styles_path)
+      # Parse styles from Grid 3's styles.xml using Nokogiri
+      styles_xml = Nokogiri::XML(File.read(styles_path))
       # Convert to a hash and return
     end
 
     def self.map_grid3_to_obf(settings, styles, grids)
       # Map Grid 3's settings, styles, and individual grids to OBF
-      # Create the OBF internal format as a hash
-      obf_internal_format = {
+      # Initialize an empty hash for OBF format
+      obf_format = {
         'format' => 'open-board-0.1',
         'buttons' => [],
         'grid' => {
@@ -60,27 +76,36 @@ module OBF
           'order' => []
         }
       }
+      # Loop through each grid and map its contents to obf_format
+      grids.each do |grid_name, grid_xml|
+        # Parse cells from the grid
+        cells = grid_xml.xpath('//Cell')
 
-      # More mapping logic here...
+        # Convert cells to OBF buttons
+        buttons = cells.map do |cell|
+          {
+            'id' => cell.attribute('Id').value,
+            'label' => cell.xpath('Caption').text,
+            # Map additional attributes...
+          }
+        end
 
-      # Return the OBF internal format
-      return obf_internal_format
+        # Apply styles to buttons
+        buttons.each do |button|
+          style_key = button['id']  # Replace with actual style key from Grid 3
+          style = styles[style_key]  # Fetch style attributes from styles hash
+          button['background_color'] = style['background_color'] if style
+          # Map additional styles...
+        end
+
+        # Add buttons to obf_format
+        obf_format['buttons'].concat(buttons)
+
+      # Map settings to obf_format (e.g., locale, name)
+      # Map styles to obf_format (e.g., background color)
+      # Loop through each grid and map its contents to obf_format
+
+      obf_format
     end
-
-    # More helper functions can go here
   end
-
-  class OBZ
-    # Existing methods...
-
-    # Create OBZ file from external content
-    def self.from_external(content, output_path)
-      # Use the external content to create an OBZ file
-      # Zip the content and save to output_path
-    end
-
-    # More existing methods...
-  end
-
-  # Existing classes and methods...
 end
